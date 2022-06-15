d9c5459f2941e520e926039d2f66857390bcfaa9
Николай Корнев <k0r1sh@yandex.ru>
2022-06-15 15:01:58 +0700

    wrap hash

--- lib/json_mapping.rb
+++ lib/json_mapping.rb
@@ -32,8 +32,8 @@ class JsonMapping
 
   def default_transforms
     {
-      'to_array' => -> (val) { [val] },
-      'to_hash' => ->(array) { array.collect{ |item| [item['key'].downcase, item['value']]}.to_h  }
+      'to_array' => -> (val) { Array.wrap(val) },
+      'to_hash' => -> (array) {  Array.wrap(array).collect{ |item| [item['key'].downcase, item['value']]}.to_h  }
     }
   end
 
